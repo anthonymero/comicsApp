27d78ccb6e8a001f52fb10980a5a17ea1eb9afa6
anthony mero <anthony@gladys.io>
2020-02-17 17:57:56 +0100

    - Create start components : (signup, signin, book-list...) - Create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth/signup',
+    component: SignupComponent
+  },
+  {
+    path: 'auth/signin',
+    component: SigninComponent
+  },
+  {
+    path: 'books',
+    component: BookListComponent
+  },
+  {
+    path: 'books/:id',
+    component: SingleBookComponent
+  },
+  {
+    path: 'books/new',
+    component: BookFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
