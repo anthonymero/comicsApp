d2da588f6ec809cc3f7961ef435d2982607a4ef9
anthony mero <anthony@gladys.io>
2020-03-11 16:06:53 +0100

    - Set authentication methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,37 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  // Create new user
+  createNewUser(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return new Promise((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(
+        () => {
+          resolve();
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  // Signin user
+  signInUser(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(
+        (user) => {
+          resolve(user);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  // Signout user
+  signOutUser() {
+    firebase.auth().signOut();
+  }
 }
